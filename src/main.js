e339ecab829e7b04ea044a2bda0cfaf282464ce5
amir <amirtaki623@gmail.com>
2025-08-24 21:33:30 +0330

    part 23

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 const list = document.querySelector(".slider .list");
 const items = document.querySelectorAll(".slider .list .item");
 const dots = document.querySelectorAll('.slider .dots li');
-const prev = document.querySelector("#prev")
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+
+let active = 0
+
+
+next.addEventListener("click", (e) => {
+    active++;
+    reloadSlider();
+})
