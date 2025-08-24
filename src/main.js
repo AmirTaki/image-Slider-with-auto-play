fc9c66ba6ca0518c156f3f9d2c525f7c32f7d9bb
amir <amirtaki623@gmail.com>
2025-08-24 21:27:39 +0330

    part 22

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
-const list = document.querySelector(".slider .list")
-const items = document.querySelectorAll(".slider .list .item")
+const list = document.querySelector(".slider .list");
+const items = document.querySelectorAll(".slider .list .item");
+const dots = document.querySelectorAll('.slider .dots li');
+const prev = document.querySelector("#prev")
